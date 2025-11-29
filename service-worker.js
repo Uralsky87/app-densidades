998b904f2662c998af36133e817de613dd889e00
Arturo Andres <arturo.anza87@gmail.com>
2025-11-29 10:50:10 +0100

    Ajustes antes de publicar en GitHub Pages

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "densidades-cache-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Activar inmediatamente versiones nuevas del SW
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
